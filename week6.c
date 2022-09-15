bd9f4b6a90917b90f6d824664b09d5493705fa29
Ditokasiditz <kasidit.samad@gmail.com>
2022-09-15 23:47:42 +0700

    fix

--- week6.c
+++ week6.c
@@ -8,13 +8,11 @@ int main(){
     scanf("%s",text);
     length = strlen(text);
  
-
     for(i=0;i<length-3;i++){
-        
         if(text[i] == 'F' || text[i] == 'f'){
             if(text[i+1] == 'r' || text[i+1] == 'R'){
                 if(text[i+2] == 'o' || text[i+2] == 'O'){
-                    if(text[i+3] || text[i+3]){
+                    if(text[i+3] == 'g' || text[i+3] == 'G'){
                         count++;
                     }
                 }
