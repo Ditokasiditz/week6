e64b1f197251184dbf6e19faec2e14bd6f412d15
Ditokasiditz <kasidit.samad@gmail.com>
2022-09-16 08:50:25 +0700

    buildmeth2

---
+++ week6_meth2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char text[30],*p;
+    int i,count=0;
+
+    scanf("%s",text);
+    p=text;
+
+    for(i=0;i<strlen(text);i++){
+        if((*p=='F'||*p=='f') && (*(p+1)=='r' || *(p+1)=='R') && 
+            (*(p+2)=='o' || *(p+2)=='O') && (*(p+3)=='g' ||*(p+3)=='G')){
+            count++;
+        }
+     p++;   
+    }
+    printf("%d",count);
+    return 0;
+    
+}
